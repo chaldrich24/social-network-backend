622598865ac9b5aaf2b6a5cdbf1ce69485821fc3
chaldrich24 <chaldrich24@gmail.com>
2022-01-06 20:39:16 -0500

    add put and delete routes for user

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
-const { getAllUsers, createUser, getUserById } = require('../../controllers/user-controller');
+const { getAllUsers, createUser, getUserById, updateUser, deleteUser } = require('../../controllers/user-controller');
 
 router.route('/')
     .get(getAllUsers)
     .post(createUser);
 
 router.route('/:id')
-    .get(getUserById);
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
 
 module.exports = router;
